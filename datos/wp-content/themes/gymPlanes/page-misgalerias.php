9d32001a3dc94ebecd4d756e4b9620fecd5536e8
manuel <manuel.lopez.fernandez97@gmail.com>
2022-02-11 12:30:20 +0100

    11 de Febrero de 2022

--- datos/wp-content/themes/gymPlanes/page-misgalerias.php
+++ datos/wp-content/themes/gymPlanes/page-misgalerias.php
@@ -12,7 +12,40 @@ get_header();?>
         <!--  Comando para detectar si hay has_post_thumbnail() -->
         <!-- Tamaños thumbail, medium, large y full -->
         <?php the_post_thumbnail('bajo', array( 'class' => 'imagendestacada')) ?>
-        <p><?php the_content()?></p>
+        
+        <?php
+            $galeria= get_post_gallery( get_the_ID(), false );
+            $fotos= explode(',',$galeria['ids']);
+            /*echo "<pre>";
+            //var_dump($galeria);
+            var_dump($galeria['ids']);
+            var_dump($fotos);
+            echo "</pre>";*/
+        ?>
+        <ul class="galeria-imagenes contenido">
+            <?php
+                $indice=1;
+                foreach($fotos as $foto):
+                    if ($indice==4 || $indice==6):
+                        $tipo='rectangu';
+                    else:
+                        $tipo='cuadrado';
+                    endif;
+                    $imagen= wp_get_attachment_image_src( $foto,$tipo )[0];
+                    $imagent= wp_get_attachment_image_src( $foto,'large' )[0];
+                    /*echo "<pre>";
+                    var_dump($imagen);
+                    echo "</pre>";*/?>
+                   
+                      <li>
+                        <a href="<?php echo $imagent; ?>" data-lightbox="mio">
+                        <img src="<?php echo $imagen?>"></a>
+                    </li>
+
+           
+               <?php $indice++; endforeach;
+            ?>
+        </ul>  
         <?php
         endwhile;
         ?>    
